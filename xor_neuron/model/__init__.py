f68d19d06cb919ffdeab96bf6e286fb579f3073e
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-02-03 17:04:23 +0900

    0203

--- xor_neuron/model/__init__.py
+++ xor_neuron/model/__init__.py
@@ -1,5 +1,5 @@
-from model.xorneuron import *
-from model.denselayer import *
-from model.conv2dlayer import *
+# from model.xorneuron import *
+# from model.denselayer import *
+# from model.conv2dlayer import *
 from model.resnet import *
 from model.resnet_xor import *
