daf46c416d780c29467677ac4148f9d3ba20dd9c
Kim-Ju-Jyeon <wngus1310@naver.com>
2022-02-14 03:10:55 +0900

    config

--- xor_neuron/shell_script/DGX/exp_resnet_DGX_2.sh
+++ xor_neuron/shell_script/DGX/exp_resnet_DGX_2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 for i in 3 4
 do
     python ../../run_resnet20.py --exp_path ../../config/resnet/0213_resnet_xor_v2_DGX/${i}.yaml &&
--- xor_neuron/shell_script/DGX/exp_resnet_DGX_4.sh
+++ xor_neuron/shell_script/DGX/exp_resnet_DGX_4.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=3
 for i in 7 8
 do
     python ../../run_resnet20.py --exp_path ../../config/resnet/0213_resnet_xor_v2_DGX/${i}.yaml &&
