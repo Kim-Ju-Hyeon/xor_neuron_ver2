c6d7e2f3867ecca8649f290e303bd4fcaf0e143a
Kim-Ju-Jyeon <wngus1310@naver.com>
2022-02-18 02:16:48 +0900

    yaml

--- xor_neuron/shell_script/DGX/exp_resnet_DGX.sh
+++ xor_neuron/shell_script/DGX/exp_resnet_DGX.sh
@@ -1,20 +1,27 @@
 #!/bin/sh
 
-export CUDA_VISIBLE_DEVICES=0
-for i in 1
-do
-    python ../../run_resnet20.py --exp_path ../../config/resnet/0218_resnet_xor_v3_DGX/${i}.yaml &
-    sleep 3
-done
-
-export CUDA_VISIBLE_DEVICES=1
-for i in 2
-do
-    python ../../run_resnet20.py --exp_path ../../config/resnet/0218_resnet_xor_v3_DGX/${i}.yaml &
-    sleep 3
-done
+#export CUDA_VISIBLE_DEVICES=0
+#for i in 1
+#do
+#    python ../../run_resnet20.py --exp_path ../../config/resnet/0218_resnet_xor_v3_DGX/${i}.yaml &
+#    sleep 3
+#done
+#
+#export CUDA_VISIBLE_DEVICES=1
+#for i in 2
+#do
+#    python ../../run_resnet20.py --exp_path ../../config/resnet/0218_resnet_xor_v3_DGX/${i}.yaml &
+#    sleep 3
+#done
 
 export CUDA_VISIBLE_DEVICES=2
+for i in 3
+do
+    python ../../run_resnet20.py --exp_path ../../config/resnet/0218_resnet_xor_v3_DGX/${i}.yaml &
+    sleep 3
+done
+
+export CUDA_VISIBLE_DEVICES=3
 for i in 3
 do
     python ../../run_resnet20.py --exp_path ../../config/resnet/0218_resnet_xor_v3_DGX/${i}.yaml &
