2e19d1445f8f9a83bba103a5f536bf8bc59ccb76
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-02-18 15:14:05 +0900

    config

--- xor_neuron/shell_script/RTX30/exp_resnet_rtx30.sh
+++ xor_neuron/shell_script/RTX30/exp_resnet_rtx30.sh
@@ -3,27 +3,22 @@
 export CUDA_VISIBLE_DEVICES=1
 for i in 1
 do
-    python ../../run_resnet20.py --exp_path ../../config/resnet/0207_resnet_quad_v2_rtx30/${i}.yaml &
+    python ../../run_resnet20.py --exp_path ../../config/resnet/0218_resnet50_v3_rtx30/${i}.yaml &
     sleep 3
 done
 
 export CUDA_VISIBLE_DEVICES=2
 for i in 2
 do
-    python ../../run_resnet20.py --exp_path ../../config/resnet/0207_resnet_quad_v2_rtx30/${i}.yaml &
+    python ../../run_resnet20.py --exp_path ../../config/resnet/0218_resnet50_v3_rtx30/${i}.yaml &
     sleep 3
 done
 
 export CUDA_VISIBLE_DEVICES=3
 for i in 3
 do
-    python ../../run_resnet20.py --exp_path ../../config/resnet/0207_resnet_quad_v2_rtx30/${i}.yaml &
+    python ../../run_resnet20.py --exp_path ../../config/resnet/0218_resnet50_v3_rtx30/${i}.yaml &
     sleep 3
 done
 
-export CUDA_VISIBLE_DEVICES=0
-for i in 4
-do
-    python ../../run_resnet20.py --exp_path ../../config/resnet/0207_resnet_quad_v2_rtx30/${i}.yaml &
-    sleep 3
-done
+-
