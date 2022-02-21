02e12448511a6e222a0d2d127fdf4723419267e9
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-02-21 17:50:20 +0900

    xavier init resnet

--- xor_neuron/shell_script/RTX30/exp_resnet_rtx30.sh
+++ xor_neuron/shell_script/RTX30/exp_resnet_rtx30.sh
@@ -3,21 +3,21 @@
 export CUDA_VISIBLE_DEVICES=0
 for i in 1
 do
-    python ../../run_resnet20.py --exp_path ../../config/resnet/0218_resnet50_xor_v3_rtx30/${i}.yaml &
+    python ../../run_resnet20.py --exp_path ../../config/resnet/0221_resnet_xor_xavier_v3_rtx30/${i}.yaml &
     sleep 3
 done
 
 export CUDA_VISIBLE_DEVICES=1
 for i in 2
 do
-    python ../../run_resnet20.py --exp_path ../../config/resnet/0218_resnet50_xor_v3_rtx30/${i}.yaml &
+    python ../../run_resnet20.py --exp_path ../../config/resnet/0221_resnet_xor_xavier_v3_rtx30/${i}.yaml &
     sleep 3
 done
 
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=3
 for i in 3
 do
-    python ../../run_resnet20.py --exp_path ../../config/resnet/0218_resnet50_xor_v3_rtx30/${i}.yaml &
+    python ../../run_resnet20.py --exp_path ../../config/resnet/0221_resnet_xor_xavier_v3_rtx30/${i}.yaml &
     sleep 3
 done
 
