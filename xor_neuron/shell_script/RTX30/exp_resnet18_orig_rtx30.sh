c9118acf0efd1a93c69af199b2e62bcb22f23b57
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-02-25 14:47:44 +0900

    shell

--- xor_neuron/shell_script/RTX30/exp_resnet18_orig_rtx30.sh
+++ xor_neuron/shell_script/RTX30/exp_resnet18_orig_rtx30.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 for i in 1 2 3 4
 do
     python ../../run_resnet20.py --exp_path ../../config/resnet/resnet18_orig/${i}.yaml &&
