ea644d707d50eae49de27905780e48706a4dc260
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-03-17 17:21:03 +0900

    resnet18 exp

--- xor_neuron/shell_script/RTX30/0315_exp_resnet18_1arg_2arg.sh
+++ xor_neuron/shell_script/RTX30/0315_exp_resnet18_1arg_2arg.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 export CUDA_VISIBLE_DEVICES=0
-for i in 1 1 1 1
+for i in 1
 do
     python ../../run_resnet20.py --exp_path ../../config/resnet/0315_resnet_1arg_rtx30/${i}.yaml --exp_num 4 &
     sleep 3
 done
 
 export CUDA_VISIBLE_DEVICES=1
-for i in 1 1 1 1
+for i in 1
 do
     python ../../run_resnet20.py --exp_path ../../config/resnet/0315_resnet_1arg_rtx30/${i}.yaml --exp_num 4 &
     sleep 3
 done
 
 export CUDA_VISIBLE_DEVICES=2
-for i in 1 1 1 1
+for i in 1
 do
     python ../../run_resnet20.py --exp_path ../../config/resnet/0315_resnet_2arg_rtx30/${i}.yaml --exp_num 4 &
     sleep 3
@@ -23,7 +23,7 @@ done
 
 
 export CUDA_VISIBLE_DEVICES=3
-for i in 1 1 1 1
+for i in 1
 do
     python ../../run_resnet20.py --exp_path ../../config/resnet/0315_resnet_2arg_rtx30/${i}.yaml --exp_num 4 &
     sleep 3
